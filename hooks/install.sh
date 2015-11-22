bc2f7fd098d961891aa3100de6cb712b7172808a
Giovanni Santini <giovannisantini93@yahoo.it>
2015-11-23 00:02:45 +0100

    Adding updates and improved hook install script

--- hooks/install.sh
+++ hooks/install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 echo "Adding commit hooks..."
 shopt -s nullglob
-	for folder in ../.git/modules/*/hooks/ ../*/.git/hooks/; do
-		for hook in *.hook; do
-		ln -sf "$(pwd)/${hook}" "${folder}/${hook%.hook}"
-		done
+old_dir=$(pwd)
+work_dir=$(dirname $(readlink -f $0))
+cd $work_dir
+for folder in ../.git/modules/*/hooks/ ../*/.git/hooks/; do
+	echo $folder
+	for hook in *.hook; do
+	ln -sf "$(pwd)/${hook}" "${folder}/${hook%.hook}"
 	done
+done
+cd $old_dir
 echo "Done!"
