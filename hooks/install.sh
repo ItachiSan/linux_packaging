e20fe467150cac91e0e62cca77002f33196f6bd7
Giovanni Santini <giovannisantini93@yahoo.it>
2018-08-08 16:46:36 +0200

    updpkg: webkit2gtk-mse Log from the packages:  upgpkg: webkit2gtk-mse 2.20.2-2  - Fix issue when building on machines with small number of cores
    Hooks updates:
     - Better installation
     - Track only the packages

--- hooks/install.sh
+++ hooks/install.sh
@@ -1,14 +1,29 @@
-#!/bin/bash
-echo "Adding commit hooks..."
+#!/bin/sh
 shopt -s nullglob
 old_dir=$(pwd)
 work_dir=$(dirname $(readlink -f $0))
+
+if [ ! -z $DEBUG ]
+then
+    OPTS='-v'
+fi
+
+echo "Adding repository hooks..."
 cd $work_dir
 for folder in ../.git/modules/*/hooks/ ../*/.git/hooks/; do
-	echo $folder
-	for hook in *.hook; do
-	ln -sf "$(pwd)/${hook}" "${folder}/${hook%.hook}"
+	echo -e "-> $folder"
+	for hook in `ls -1 *.hook | grep -v _master`
+    do
+        ln -sf $OPTS "$(pwd)/${hook}" "${folder}${hook%.hook}"
 	done
 done
+
+echo "Adding master repository hooks..."
+cd $work_dir
+for hook in `ls -1 *.hook | grep _master`
+do
+    ln -sf $OPTS "$(pwd)/${hook}" "../.git/hooks/${hook%.hook}"
+done
+
 cd $old_dir
 echo "Done!"
