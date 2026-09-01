9600651fa7eba20eba255aee3aaa8bec8a8eda92
Giovanni Santini <giovannisantini93@yahoo.it>
2026-09-01 21:52:25 +0200

    updpkg:   9router ferdium-electron libjxl-0.11 obs-plugin-browser popcorntime xmrig-mo-cuda updtools: check_updates.sh
    Log from the modified packages:
    
     upgpkg: 9router 0.5.59-1
     - Version bump.
    
     multiple packages: add .nvchecker.toml
     Allow for version check via `pkgctl version check`
    
     upgpkg: xmrig-mo-cuda 6.22.1mo1-2
     - Make CUDA build more universal, should be future-proof
     - Add support for `pkgctl version check`
    
    Tools updates:
    * tools/check_updates.sh: use `pkgctl version check`

--- tools/check_updates.sh
+++ tools/check_updates.sh
@@ -4,14 +4,13 @@ _code_dir=`dirname $_my_dir`
 
 pushd $_code_dir > /dev/null
 
-# Refresh the "old.json" file
-python3 tools/old_refresher.py
+# Collect all packages with PKGBUILD
+packages=
+for dir in */; do
+    [[ -f "${dir}PKGBUILD" ]] && packages="$packages ${dir%%/}"
+done
 
-# Check for updates
-nvchecker -c tools/nvchecker.toml
-
-# Report updated
-echo "# Packages to update:"
-nvcmp -c tools/nvchecker.toml
+# Check for updates on all packages with a PKGBUILD
+pkgctl version check -v $packages
 
 popd > /dev/null
